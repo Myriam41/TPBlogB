acc1cd573fd7bea8926da8d1669a2293fd473886
Myriam41 <37068528+Myriam41@users.noreply.github.com>
2018-05-09 10:02:45 +0200

    Update Article.php
    description doc

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,115 +2,206 @@
 
 namespace App\Entity;
 
+/**
+ * Class Article
+ * 
+ * Les attribus de la class sont private pour que la class ne puisse pas perdre sont identité. 
+ * Ils sont utilisables grâce aux getters
+ */
+
 class Article
 {
-/*Les attribus de la class sont private 
-pour que la class ne puisse pas perdre sont identité. 
-Ils sont utilisables car aux fonctionis get.*/
+    /**
+     * @var int primary key autoincrémentée
+    */
     private $id;
-    
+
+    /**
+     * @var string article's title
+     */
     private $title;
 
+    /**
+     * @var string article's tintroduction
+     */
     private $introduction;
 
+    /**
+     * @var string article's content
+     */
     private $content;
 
+    /**
+     * @var int article's titlecreation date
+     */
     private $createdAt;
 
+    /**
+     * @var int article's update date
+     */
     private $updateAt;
 
+    /**
+     * @param string $data use function hydrate
+     */
     public function __construct($data)
     {
         $this->hydrate($data);
     }
 
+    /**
+     * @param string $data hydrate attributs with data
+     */
     public function hydrate($data)
     {
-// On test si data est un tableau
+    
+        /**
+         * if $data is an array. The value of each column is retrieved 
+         */
         if(is_array($data))
         {
             if (isset($data['id']))
- // j'appelle ma fonction grâce à this. la valeur de chaque colonne est récupérée
-            {
+            {   
+                /**
+                 * hydration id
+                 * I call my function with this. 
+                 */
                 $this->setId($data['id']);
             }
-
+             
             if (isset($data['title']))
             {
+                /**
+                 * hydration title
+                 */
                 $this->setTitle($data['title']);
             }
             
             if (isset($data['content']))
             {
+                /**
+                 * hydration content
+                 */
                 $this->setContent($data['content']);
             }
 
             if (isset($data['introduction']))
             {
+                 /**
+                 * hydration introduction
+                 */
                 $this->setIntroduction($data['introduction']);
             }
 
             if (isset($data['createAt']))
             {
+                 /**
+                 * hydration createAt
+                 */
                 $this->setcreateAt($data['createAt']);
             }
 
-            if (isset($data['update']))
+            if (isset($data['updateAt']))
             {
-                $this->setUpdate(['update']);
+                 /**
+                 * hydration UpdateAt
+                 */
+                $this->setUpdateAt(['updateAt']);
             }
         }  
     }
 
-//functions pour id
+    /**
+     * @return int id
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     */
     private function setId($id)
     {
         $this->id = $id;
     }
 
-//functions pour title
+     /**
+     * @return string title
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     * @param string $title
+     */
     public function setTitle($title)
     {
         $this->title = $title;
     }
 
-//functions pour introduction
+     /**
+     * @return string introduction
+     */
     public function getIntroduction()
     {
         return $this->introduction;
     }
 
+    /**
+     * @param string $introduction
+     */
     public function setIntroduction($introduction)
     {
         $this->introduction = $introduction;
     }
 
-//functions pour content
+    /**
+     * @return string content
+     */
     public function getcontent()
     {
         return $this->createdAt;
     }
 
+    /**
+     * @param string $content
+     */
     public function setContent($content)
     {
         $this->content = $content;
     }
-//functions pour updateAt
+
+    /**
+     * @return int createAt
+     */
+    public function getcreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * @param int $createAt
+     */
+    public function setcreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+    }
+
+     /**
+     * @return int updateAt
+     */
     public function getupdateAt()
     {
         return $this->updateAt;
     }
 
+    /**
+     * @param int $undateAt
+     */
     public function setupdateAt($updateAt)
     {
         $this->updateAt = $updateAt;
