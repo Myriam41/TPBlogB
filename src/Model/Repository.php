59a106167b14fae3909a7f108238c3b9fe6688c5
Myriam41 <37068528+Myriam41@users.noreply.github.com>
2018-05-09 10:05:10 +0200

    Description doc
    Début de la mise en place de la doc

--- src/Model/Repository.php
+++ src/Model/Repository.php
@@ -1,29 +1,46 @@
 <?php
 
 namespace App\Model;
-use PDO;
 
-// class abstraite pour qu'elle ne soit pas instenciée. Seulement ses filles doivent pouvoir l'être.
+use \PDO;
+
+/**
+ * class repository
+ * 
+ * @abstract 
+ * class abstraite pour qu'elle ne soit pas instenciée. Seulement ses filles doivent pouvoir l'être.
+ */
 abstract class Repository
 {
-    protected $db;
-
-    public function __construct()
+    /**
+     * @var int 
+     * data to connect with database
+     */
+    private $db;
+    
+    /**
+     * @return db
+     */
+    protected function getDb()
     {
-        try
+        if ($this->db === NULL)
         {
-            $this->db = new \PDO('mysql:dbname=tpblog;host=localhost;charset=utf8', 'root', '' );
+            try
+            {
+                $db = new \PDO('mysql:dbname=tpblog;host=localhost;charset=utf8', 'root', '' );
 
-            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        }
-        catch(PDOException $e)
-        {
-            die('Echec lors de la connexion : '.$e->getMessage());
-        }
-    }
+                $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
-    protected function getPDO()
-    {
+                $this->db = $db;
+
+                return $this->db;
+            }
+            catch(PDOException $e)
+            {
+                die('Echec lors de la connexion : '.$e->getMessage());
+            }
+        }
+
         return $this->db;
     }
 }
