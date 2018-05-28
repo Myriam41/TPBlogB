d9edfc0d9608a7a18b032031bb7c79f1c7d045c6
Myriam41 <37068528+Myriam41@users.noreply.github.com>
2018-05-28 10:13:30 +0200

    28 mai 2018

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -1,43 +1,43 @@
 <?php
-namespace App\Model;
-function getComments($articleID)
+namespace App\Repository;
+
+use App\Model\Repository;
+use App\Entity\Comment;
+
+class CommentRepository extends Repository
 {
-    //connexion à la base de données
-    try
+    function getByArticleId($articleID)
     {
-        $db = new PDO('mysql:host=localhost;dbname=tpblog;charset=utf8', 'root', '');
-
+        $db = $this->getDb();
+        $articleId = $_GET['id'];
+    
+        // récupération des 10 derniers comments d'un article 
+        $reqSelect = 'SELECT id, articleId, author, contmessage, DATE_FORMAT(createdAt, \'%d/%m/%Y à %Hh%imin%ss\') AS date_comment_fr';
+        $reqFrom = ' FROM comments';
+        $reqWhere = ' WHERE articleId = :articleId ORDER BY createdAt LIMIT 0, 10';
+        $req = $db->prepare($reqSelect . $reqFrom . $reqWhere);
+        $req->bindparam(':articleId', $articleId, \PDO::PARAM_INT);
+        $req->execute();
+        
+        $comments = [];
+        while ($comment = $req->fetch())
+        {
+            $comments[] = new Comment($comment);
+        }
+        return $comments;
     }
-    catch(Exception $e)
+    
+    function getComment($commentID)
     {
-        die('Error : '.$e->getMessage());
+        $db = $this->getDb();
+    
+        // recherche de l'article demandé
+        $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%im%ss\') AS date_comment_fr FROM comments');
+        $req->execute(array($articleId));
+        $comment = [];
+        $comment = $req->fetch();
+    
+        return $comment;
+    
     }
-
-    // récupération des 10 derniers comments d'un article 
-    $req = $db->prepare('SELECT id, post_id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\' AS date_comment_fr FROM comments  WHERE post_id = ? ORDER BY comment_date LIMIT 0, 10');
-    $req->execute(array());
-    $articles = $req->fetch();
-
-    return $comments;
-}
-
-function getComment($commentID)
-{
-    try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=tpblog;charset=utf8', 'root', '');
-
-    }
-    catch(Exception $e)
-    {
-        die('Error : '.$e->getMessage());
-    }
-
-    // recherche de l'article demandé
-    $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%im%ss\' AS date_comment_fr FROM comments');
-    $req->execute(array($articleID));
-    $comment = $req->fetch();
-
-    return comment;
-
 }
