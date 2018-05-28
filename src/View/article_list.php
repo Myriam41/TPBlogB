0efb40e7a0bae159519b8747e7464f0385182c28
Myriam41 <37068528+Myriam41@users.noreply.github.com>
2018-05-28 10:14:24 +0200

    28 mai 2018

--- src/View/article_list.php
+++ src/View/article_list.php
@@ -1,35 +1,37 @@
 <?php
-    /* définition du titre de la page qui sera 
+
+/* définition du titre de la page qui sera
     insérée dans le template en fin de script*/
 
     $title = 'Les articles de Mimi';
 
     /* la fonction ob_start mémorise la sortie HTML 
     qui sera récupérée à la fin du script par la fonction ob_get_clean */
-
-    ob_start();?>
-        <h1> Les 10 derniers articles </h1>
-
-    <?php // affichage de la liste des articles
     
+    ob_start();?>
+
+        <h1> Les 10 derniers articles </h1>
+    <?php 
+    
+    // affichage de la liste des articles
     foreach ($articles as $article)
     {
     ?>
-        <div class="news">
-            <h3>
-                <? htmlspecialchars($article->getTitle()) ;?>
-            </h3>
-            <br/>
-            <p>
-                <?//on affiche le contenu de l'article
-                htmlspecialchars($article->getContent());?>
-            <br/>
-                <em><a href="articleView.php"=<? $data['id']; ?>">Commentaires</a></em>
-            </p>
-        </div>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($article->getTitle()); ?>
+        </h3>
+        <p>
+            <?= //on affiche le contenu de l'article
+            htmlspecialchars($article->getcontent()); ?>
+        <br/>
+               <em><a href="index.php?id=<?= $article->getId()?>&amp;page=articleView">commentaires</a></em>
+        </p>
+    </div>
     <?php
+        
     }
 
     $content = ob_get_clean();
+    include 'templates/default.php';
 
-    require('templates/default.php');
