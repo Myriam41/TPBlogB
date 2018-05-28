053419cc545a65754288f90c6d88501d2eb04025
Myriam41 <37068528+Myriam41@users.noreply.github.com>
2018-05-28 10:07:14 +0200

    28 mai 2018

--- public/index.php
+++ public/index.php
@@ -1,30 +1,30 @@
 <?php
-//l'index est mon routeur
 
 //j'appelle ce qui va permettre autoloading des fichiers de l'application que je vais nommer ensuite
-include_once'../vendor/autoload.php';
+require_once'../vendor/autoload.php';
 
 // charge les fichiers de l'appli
 use App\Controller\ArticleController;
 use App\View;
-use App\Entity;
-
+use App\Entity\Article;
+use App\Controller\CommentController;
+use App\Repository\CommentRepository;
 
 //le chemin prit suivant les indications récupérées dans URL
 //si le paramètre action existe on test le paramètre sinon on charge la liste des articles.
 
-if(isset($_GET['page']))
-    {
-        $p = $_GET['page'];
-    }
-      
-else
-    {
-        $p = 'listarticle';
-    }
+if (isset($_GET['page'])) {
+    $p = $_GET['page'];
+} else {
+    $p = 'listArticle';
+}
 
-if($p === 'listarticle')
-{
-    $controller = new ArticleController();
-    $controller->ListArticles();
+if ($p === 'listArticle') {
+    $articleController = new ArticleController();
+    $articleController->ListArticles();
+}
+
+if ($p === 'articleView') {
+    $contArticle = new ArticleController();
+    $contArticle->article();
 }
