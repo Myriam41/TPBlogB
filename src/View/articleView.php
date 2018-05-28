7619522f14d33b9472d1989a0c64fdceed652491
Myriam41 <37068528+Myriam41@users.noreply.github.com>
2018-05-28 10:15:56 +0200

    28 mai 2018

--- src/View/articleView.php
+++ src/View/articleView.php
@@ -1,40 +1,52 @@
-<? // définition du titre de la page qui sera insérée dans le template en fin de script ?>
+<?php
+namespace App\View;
 
-<?php $title = 'Un article de Mimi'; ?>
+// définition du titre de la page qui sera inséré dans le template en fin de script
+$title = 'Un article de Mimi';
 
-<? /* la fonction ob_start mémorise la sortie HTML 
-qui sera récupérée à la fin du script par la fonction ob_get_clean */ ?>
-
-<?php ob_start(); ?>
-    <h1> Un super article </h1>
-    <p> Donnez votre avis </p>
-
-<? // insérer le formulaire pour l'envois des comentaires à faire plus tard ?>
-
-<?php // affichage d'un article avec ses commentaires
-while ($data = $article->fetch())
-{
+// la fonction ob_start mémorise la sortie HTML
+//qui sera récupérée à la fin du script par la fonction ob_get_clean
+ob_start();
 ?>
+
+    <h1> Un super article </h1>
+    <p> Vous donnerez votre avis plus tard ! </p>
+
+<?php
+// insérer le formulaire pour l'envois des contentaires à faire plus tard
+try{
+    echo $req;
+}
+catch (Exception $e){
+    echo 'exception reçue : ', $e->getMessage(); 
+}
+// affichage d'un article
+foreach($onearticle as $article)
+{
+    ?>
+
     <div class="news">
         <h3>
-            <? htmlspecialchars($data['title']) ;?>
-            <em>le <? $data['creation_date.fr'] ;?></em>
+            <?= htmlspecialchars($article->getTitle()) ;?>
+            <em>le <?= $article->getCretaedAt() ; ?></em>
+            
         </h3>
 
         <p>
         <?php
             //on affiche le contenu de l'article
-            echo nl12br(htmlspecialchars($data['content']));
-        ?>
+            echo nl12br(htmlspecialchars($article->getContent())) ; ?>
         <br/>
-            <em><a href="Commentlist.php?article=<? $data['id']; ?>">Commentaires</a></em>
+    
         </p>
     </div>
-<?php
+    <?php
 }
-$article->closeCursor();
-?>
-<?php $content = ob_get_clean(); ?>
+foreach($comments as $comment)
+{
+    echo nl12br(htmlspecialchars($comment->getMessage()));
+}
 
-<?php require('src/view/template.php'); ?>
+$content = ob_get_clean();
 
+require 'templates/default.php';
